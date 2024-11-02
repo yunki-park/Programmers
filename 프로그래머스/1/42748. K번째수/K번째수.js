8fdc2b7ab4ac2445849871c972921c27486b9804
yg <56058845+yunki-park@users.noreply.github.com>
2024-11-02 22:22:11 +0900

    [level 1] Title: K번째수, Time: 0.16 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.js
+++ 프로그래머스/1/42748. K번째수/K번째수.js
@@ -1,20 +1,10 @@
 function solution(array, commands) {
-    var answer = [];
-    
-    for (const command of commands) {
-        // 1. 범위 내 요소 자르기
-        const tmpArray = array.slice(command[0] - 1, command[1]);
-        console.log(tmpArray);
-        // 2. 정렬 (sort 함수는 기본적으로 문자열 비교이므로 별도 비교 함수 작성)
-        tmpArray.sort((a, b) => a - b);
-        // console.log(tmpArray);
-        // 3. n번째 요소 찾기
-        const n = command[2];
-        const nthElement = tmpArray[n - 1];
-        // console.log(tmpArray);
-        // 4. 결과 배열에 담기
-        answer.push(nthElement);
-    }
-    
-    return answer;
+    return commands.map(command => {
+        const [sPosition, ePosition, position] = command
+        const newArray = array
+            .filter((value, fIndex) => fIndex >= sPosition - 1 && fIndex <= ePosition - 1)
+            .sort((a,b) => a - b)    
+
+        return newArray[position - 1]
+    })
 }
