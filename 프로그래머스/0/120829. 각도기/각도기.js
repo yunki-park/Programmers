d6d139040c1f64d9d557996268668d47774002ee
yg <56058845+yunki-park@users.noreply.github.com>
2024-11-06 20:57:05 +0900

    [level 0] Title: 각도기, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120829. 각도기/각도기.js
+++ 프로그래머스/0/120829. 각도기/각도기.js
@@ -1,16 +1,3 @@
 function solution(angle) {
-    if (angle < 90) {
-        return 1;
-    }
-    
-    if (angle === 90) {
-        return 2;
-    }
-    
-    if (angle < 180) {
-        return 3;
-    }
-    
-    // angle = 180
-    return 4;
+    return [0, 90, 91, 180].filter(a => angle >= a).length;
 }
