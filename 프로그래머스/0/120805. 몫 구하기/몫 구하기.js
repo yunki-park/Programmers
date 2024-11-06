e449a48c6ffe239897f5e3c218f867a1cb62b393
yg <56058845+yunki-park@users.noreply.github.com>
2024-11-06 20:45:51 +0900

    [level 0] Title: 몫 구하기, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120805. 몫 구하기/몫 구하기.js
+++ 프로그래머스/0/120805. 몫 구하기/몫 구하기.js
@@ -1,3 +1,5 @@
 function solution(num1, num2) {
-    return Math.floor(num1 / num2);
+    // return Math.floor(num1 / num2);
+    // 정수부분 반환 (소수점 제거) - 음수든 양수든 단순히 정수부분만 반환
+    return Math.trunc(num1 / num2);
 }
