4acf7e011fa5a352b9a848b3f3e759f784c6ddde
nemo-dream <56058845+yunki-park@users.noreply.github.com>
2025-04-08 00:00:47 +0900

    [level 2] Title: 올바른 괄호, Time: 31.27 ms, Memory: 38 MB -BaekjoonHub

---
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.js
@@ -0,0 +1,16 @@
+function solution(s){
+    const stack = [];
+    
+    for (let char of s) {
+        if (char === '(') {
+            stack.push(char);
+        } else {
+            if (stack.length === 0) {
+                return false;
+            }
+            stack.pop();
+        }
+    }
+
+    return stack.length === 0;
+}
