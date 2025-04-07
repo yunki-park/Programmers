e7abebf8078cf5d75adef6b16f7f52c043607892
nemo-dream <56058845+yunki-park@users.noreply.github.com>
2025-04-07 17:53:20 +0900

    [level 0] Title: 콜라츠 수열 만들기, Time: 0.11 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/181919. 콜라츠 수열 만들기/콜라츠 수열 만들기.js
@@ -0,0 +1,11 @@
+function solution(n) {
+    var answer = [];
+    answer.push(n);
+
+    while (n !== 1) {
+        n = n % 2 === 0 ? n / 2 : 3 * n + 1;
+        answer.push(n);
+    }
+    
+    return answer;
+}
