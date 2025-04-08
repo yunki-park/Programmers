8fd06258825f9008e5e91e575f3fc386cc5e6439
nemo-dream <56058845+yunki-park@users.noreply.github.com>
2025-04-08 22:11:44 +0900

    [level 2] Title: 피보나치 수, Time: 119.28 ms, Memory: 42.2 MB -BaekjoonHub

--- 프로그래머스/2/12945. 피보나치 수/피보나치 수.js
+++ 프로그래머스/2/12945. 피보나치 수/피보나치 수.js
@@ -1,13 +1,12 @@
 function solution(n) {
-    // const dp = [0, 1];
-    let num1 = 0;
-    let num2 = 1;
+    let num1 = 0n;
+    let num2 = 1n;
     
-    for (let i = 2; i <= n; i++) {
-        const num3 = (num1 + num2) % 1234567;
+    for (let i = 2n; i <= n; i++) {
+        const num3 = num1 + num2;
         num1 = num2;
         num2 = num3;
     }
     
-    return num2;
+    return num2 % 1234567n;
 }
