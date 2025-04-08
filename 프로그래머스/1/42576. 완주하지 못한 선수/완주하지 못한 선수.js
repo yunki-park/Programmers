7b0330a5fb71991b37c702f91ab6429ae52133d7
nemo-dream <56058845+yunki-park@users.noreply.github.com>
2025-04-08 16:07:54 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 66.51 ms, Memory: 66.8 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
@@ -1,30 +1,21 @@
 function solution(participant, completion) {
-    const completionSet = new Set(completion);
-    const uniquePerson = participant.find(item => !completionSet.has(item));
-
-    if (!uniquePerson) {
-        return refind(participant, completion);
+    const map = new Map(); // key: name, value: count
+    let answer = "";
+    
+    for (const name of participant) {
+        map.set(name, (map.get(name) || 0) + 1);
     }
-    return uniquePerson;
-}
-
-function refind(participant, completion) {
-    const countMap = new Map();
-
-    // construct countMap
-    participant.forEach(name => {
-        countMap.set(name, (countMap.get(name) ?? 0) + 1);
-    })
-
-    // update countMap
-    completion.forEach(name => {
-        countMap.set(name, countMap.get(name) - 1);
-    });
-
-    // find not 0 element
-    for (const [name, count] of countMap.entries()) {
-        if (count) {
-            return name;
+    
+    for (const name of completion) {
+        map.set(name, map.get(name) - 1);
+    }
+    
+    for (const [name, count] of map) {
+        if (count > 0) {
+            answer = name;
+            break;
         }
     }
+    
+    return answer;
 }
