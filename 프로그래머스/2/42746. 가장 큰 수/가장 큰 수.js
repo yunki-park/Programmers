1db9594935bfb964ddce58514832a7d8905c901a
nemo-dream <56058845+yunki-park@users.noreply.github.com>
2025-04-08 17:18:25 +0900

    [level 2] Title: 가장 큰 수, Time: 167.18 ms, Memory: 44.7 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.js
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.js
@@ -1,18 +1,6 @@
 function solution(numbers) {
-    let answer = '';
-    
-    // 1. number -> string 변환하여 새 배열 생성
-    const numberStrs = numbers.map(number => number.toString());
-    // console.log(numberStrs);
-    
-    // 2. 기준 함수에 따라 정렬
-    numberStrs.sort((a, b) => (b + a) - (a + b));
-    // console.log(numberStrs);
-    
-    // 3. 문자열 합친 형태로 변환
-    for (const str of numberStrs) {
-        answer += str;
-    }
+    const sorted = numbers.map(String).sort((a, b) => (b + a) - (a + b));
+    const answer = sorted.join('');
     
     return answer[0] === '0' ? '0' : answer;
 }
