a08faf89a71043d2a33d5f407cdb0ca1b5907b00
nemo-dream <56058845+yunki-park@users.noreply.github.com>
2025-04-11 23:21:08 +0900

    [level 1] Title: K번째수, Time: 0.12 ms, Memory: 33 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.js
+++ 프로그래머스/1/42748. K번째수/K번째수.js
@@ -1,10 +1,29 @@
-function solution(array, commands) {
-    return commands.map(command => {
-        const [sPosition, ePosition, position] = command
-        const newArray = array
-            .filter((value, fIndex) => fIndex >= sPosition - 1 && fIndex <= ePosition - 1)
-            .sort((a,b) => a - b)    
+// 개요
+// 배열 i~j 자르고 정렬 -> k번째 수
 
-        return newArray[position - 1]
-    })
+// 조건
+// array.length <= 100
+// commands.length <= 50
+
+// 입력
+// array: 정수 배열 (1~100)
+// commands: 2차 배열, [i, j, k]
+
+// 출력
+// 배열, 배열 i~j 자르고 정렬 -> k번째 수
+
+function solution(array, commands) {
+    const result = [];
+    
+    for (let command of commands) {
+        const [i, j, k] = command;
+        
+        const sliced = array.slice(i - 1, j);
+        const sorted = sliced.sort((a, b) => a - b);
+        
+        const kNumber = sorted[k - 1];
+        result.push(kNumber);
+    }
+    
+    return result;
 }
--- 프로그래머스/1/42748. K번째수/README.md
+++ 프로그래머스/1/42748. K번째수/README.md
@@ -1,10 +1,10 @@
 # [level 1] K번째수 - 42748 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42748?language=javascript) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42748) 
 
 ### 성능 요약
 
-메모리: 33.6 MB, 시간: 0.16 ms
+메모리: 33 MB, 시간: 0.12 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 11월 02일 22:22:09
+2025년 04월 11일 23:21:06
 
 ### 문제 설명
 
