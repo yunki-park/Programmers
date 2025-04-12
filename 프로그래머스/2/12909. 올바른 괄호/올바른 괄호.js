ddd9f1cc48ceea57f78d53935b3c25a0d66100f1
nemo-dream <56058845+yunki-park@users.noreply.github.com>
2025-04-12 20:09:52 +0900

    [level 2] Title: 올바른 괄호, Time: 27.63 ms, Memory: 37.2 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.js
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.js
@@ -1,16 +1,31 @@
-function solution(s){
+// 개요
+// 문자열 s에 포함된 () 괄호 짝 일치 여부 구하기
+
+// 조건
+// s <= 10^5
+
+// 입력
+// 문자열 s, ("(", ")")
+
+// 출력
+// Boolean, 괄호 짝 일치 여부
+
+function solution(s) {    
     const stack = [];
     
-    for (let char of s) {
+    if (s[0] === ')') {
+        return false;
+    }
+
+    for (let i = 0; i < s.length; i++) {
+        const char = s[i];
+        
         if (char === '(') {
             stack.push(char);
         } else {
-            if (stack.length === 0) {
-                return false;
-            }
             stack.pop();
         }
     }
-
+    
     return stack.length === 0;
 }
