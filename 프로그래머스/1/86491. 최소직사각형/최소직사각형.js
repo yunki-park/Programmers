1acf2d2f28b5fcb4f1df521727272b063f6fdaa0
nemo-dream <56058845+yunki-park@users.noreply.github.com>
2025-04-13 00:47:54 +0900

    [level 1] Title: 최소직사각형, Time: 10.48 ms, Memory: 39.5 MB -BaekjoonHub

--- 프로그래머스/1/86491. 최소직사각형/최소직사각형.js
+++ 프로그래머스/1/86491. 최소직사각형/최소직사각형.js
@@ -1,3 +1,16 @@
+// 개요
+// 여러 크기의 명함들을 수납할 수 있는 최소 크기 지갑 크기 구하기
+
+// 조건
+// sizes.length <= 10^4 (1 ~ 10^4)
+// sizes: [w, h] (1 <= w, h <= 10^3)
+
+// 입력
+// sizes: 배열, 각 명함의 [w, h]
+
+// 출력
+// 정수, 지갑 크기 (넓이)
+
 function solution(sizes) {
     const widths = [];
     const heights = [];
@@ -6,6 +19,7 @@ function solution(sizes) {
         widths.push(Math.max(size[0], size[1]));
         heights.push(Math.min(size[0], size[1]));
     }
+    
     widths.sort((a, b) => b - a);
     heights.sort((a, b) => b - a);
     
