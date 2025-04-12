303b6f0aaec862d65e764125193d0193eb98a8b4
nemo-dream <56058845+yunki-park@users.noreply.github.com>
2025-04-12 13:58:36 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 14.24 ms, Memory: 83 MB -BaekjoonHub

--- 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.js
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.js
@@ -1,14 +1,42 @@
-function solution(arr)
-{
-    const answer = [];
-    
-    for (const item of arr) {
-        const answerTop = answer[answer.length - 1];
+// 개요
+// 한 자리 정수 배열에서 연속 숫자 제거한 배열 구하기
+
+// 조건
+// arr <= 10^6
+
+// 입력
+// 한 자리 정수 배열 (0~9)
+
+// 출력
+// 한 자리 정수 배열 (0~9)
+// 동일 숫자 연속된 경우 제거
+
+function solution(arr) {
+    const resultArr = [];
+
+    for (let i = 0; i <= arr.length - 1; i++) {
+        const latest = resultArr[resultArr.length - 1];
+        const current = arr[i];
         
-        if (answerTop !== item) {
-            answer.push(item);
+        if (latest !== current) {
+            resultArr.push(current);
         }
     }
     
-    return answer;
+    return resultArr;
 }
+
+// function solution(arr)
+// {
+//     const answer = [];
+    
+//     for (const item of arr) {
+//         const answerTop = answer[answer.length - 1];
+        
+//         if (answerTop !== item) {
+//             answer.push(item);
+//         }
+//     }
+    
+//     return answer;
+// }
