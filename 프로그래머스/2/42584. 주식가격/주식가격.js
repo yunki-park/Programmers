8eb6167dfead98fceb2dcffe3510afcd341a27d5
nemo-dream <56058845+yunki-park@users.noreply.github.com>
2025-04-13 00:20:45 +0900

    [level 2] Title: 주식가격, Time: 32.41 ms, Memory: 41.9 MB -BaekjoonHub

---
+++ 프로그래머스/2/42584. 주식가격/주식가격.js
@@ -0,0 +1,32 @@
+// 개요
+// 주식 가격이 초 단위로 기록된 배열이 주어질 때, 가격이 떨어지지 않은 기간 구하기
+
+// 조건
+// prices <= 10^4 (1 ~ 10^4)
+// prices.length < 10^5 (2 ~ 10^5)
+
+// 입력
+// prices: 정수 배열
+
+// 출력
+// 배열, 각 가격이 떨어지지 않은 시간
+
+function solution(prices) {
+    const timeArray = [];
+    
+    for (let i = 0; i < prices.length - 1; i++) {
+        let count = 1;
+        
+        for (let j = i + 1; j < prices.length; j++) {
+            if (prices[i] <= prices[j] && j !== prices.length - 1) {
+                count++;
+            } else {
+                break;
+            }
+        }
+        timeArray.push(count);
+    }
+    timeArray.push(0);
+    
+    return timeArray;
+}
