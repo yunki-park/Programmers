0a09c7edbbda5b554d09da06300185bd3001d322
nemo-dream <56058845+yunki-park@users.noreply.github.com>
2025-04-13 15:54:37 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 102.33 ms, Memory: 58.3 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
@@ -1,21 +1,27 @@
+// 개요
+// 마라톤에 참여한 선수 중 완주하지 못한 한 명의 선수 찾기
+
+// 조건
+// 1 <= participant.lenth <= 10^5
+// completion.length = participant.length - 1
+// 동명이인 존재
+
+// 입력
+// participant: 문자열 배열, 참여자 목록
+// completion: 문자열 배열, 완주자 목록
+
+// 출력
+// 문자열, 완주하지 못한 선수 이름
+
 function solution(participant, completion) {
-    const map = new Map(); // key: name, value: count
-    let answer = "";
+    participant.sort();
+    completion.sort();
     
-    for (const name of participant) {
-        map.set(name, (map.get(name) || 0) + 1);
-    }
-    
-    for (const name of completion) {
-        map.set(name, map.get(name) - 1);
-    }
-    
-    for (const [name, count] of map) {
-        if (count > 0) {
-            answer = name;
-            break;
+    for (let i = 0; i < completion.length; i++) {
+        if (participant[i] !== completion[i]) {
+            return participant[i];
         }
     }
     
-    return answer;
+    return participant[participant.length - 1];
 }
