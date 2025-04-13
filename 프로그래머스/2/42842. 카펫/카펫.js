b79ffd3ea88546fdcadc4d687371dd5d3bb3de0b
nemo-dream <56058845+yunki-park@users.noreply.github.com>
2025-04-13 12:48:10 +0900

    [level 2] Title: 카펫, Time: 0.17 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.js
+++ 프로그래머스/2/42842. 카펫/카펫.js
@@ -1,20 +1,37 @@
+// 개요
+// 노란색, 갈색 타일 개수를 기억하는 상태에서 카펫 가로, 세로 구하기
+
+// 조건
+// 가로 >= 세로
+
+// 입력
+// brown: 정수 (8 ~ 5*10^3)
+// yellow: 정수 (1 ~ 2*10^6)
+
+// 출력
+// [가로, 세로] 정수 배열
+
 function solution(brown, yellow) {
-    // const yWidth, yHeight;
-    // const yellow = yWidth * yHeight;
-    // const yHeight = yellow / yWidth;
-    // const brown = 4 + 2 * (yWidth + yellow / yWidth);
-    // 2 * yWidth * yWidth - (brown - 4) * yWidth + 2 * yellow = 0;
+    const resultSize = [0, 0]; // [w, h]
     
-    const a = 2;
-    const b = 4 - brown;
-    const c = 2 * yellow;
-    const discriminant = b * b - 4 * a * c;
-    const root1 = (-b + Math.sqrt(discriminant)) / (2 * a);
-    const root2 = (-b - Math.sqrt(discriminant)) / (2 * a);
-    const yWidth = Math.max(root1, root2);
-    const yHeight = Math.min(root1, root2);
-    
-    // console.log(root1, root2);
-    
-    return [yWidth + 2, yHeight + 2];
+    // w * h = brown + yellow (w >= h)
+    // h^2 <= brown + yellow
+    // h <= sqrt(brown + yellow)
+    //
+    // (w - 2) * (h - 2) = yellow
+    const size = brown + yellow;
+    for (let height = 3; height <= Math.sqrt(size); height++) {
+        if (size % height !== 0) {
+            continue;
+        }
+        
+        const width = size / height;
+        if ((width -2) * (height - 2) === yellow) {
+            resultSize[0] = width;
+            resultSize[1] = height;
+            break;
+        }
+    }
+
+    return resultSize;
 }
